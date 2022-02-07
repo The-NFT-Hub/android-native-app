e09746c1cd5ba7f9e11a5fc724d5cb233fa292aa
Jaap <jaap@mrproper.dev>
2022-02-07 17:56:14 +0100

    Add Ktor

--- app/app/src/main/java/nl/gewoonjaap/nfthub/ProfileActivity.kt
+++ app/app/src/main/java/nl/gewoonjaap/nfthub/ProfileActivity.kt
@@ -1,6 +1,5 @@
 package nl.gewoonjaap.nfthub
 
-import android.R.attr.label
 import android.content.ClipData
 import android.content.ClipboardManager
 import android.content.Context
@@ -8,6 +7,9 @@ import android.os.Bundle
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import io.ktor.client.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
 
 
 class ProfileActivity : AppCompatActivity() {
@@ -15,12 +17,13 @@ class ProfileActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
 
-        val address = intent.getStringExtra(WALLET_ADDRESS)
-
+        var address = intent.getStringExtra(WALLET_ADDRESS)
+        if(address == null || address.isEmpty()) address = "Unknown"
         val addressTextView = findViewById<TextView?>(R.id.WalletAddress)
             addressTextView.apply {
             text = address
         }
+
         addressTextView.setOnClickListener {
             val clipboard = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
             val clip = ClipData.newPlainText("Copied Text", address)
@@ -28,5 +31,6 @@ class ProfileActivity : AppCompatActivity() {
 
             Toast.makeText(this, "Copied Wallet Address", Toast.LENGTH_LONG).show()
         }
+
     }
 }
