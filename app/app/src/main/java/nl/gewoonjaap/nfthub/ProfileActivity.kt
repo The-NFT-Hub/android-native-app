9c63871171c65abc7a0c449fc10a764e4dca30ea
Jaap <33700526+GewoonJaap@users.noreply.github.com>
2022-02-08 18:01:05 +0100

    Random Profile image

--- app/app/src/main/java/nl/gewoonjaap/nfthub/ProfileActivity.kt
+++ app/app/src/main/java/nl/gewoonjaap/nfthub/ProfileActivity.kt
@@ -4,9 +4,11 @@ import android.content.ClipData
 import android.content.ClipboardManager
 import android.content.Context
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
 import kotlinx.coroutines.*
 import nl.gewoonjaap.nfthub.data.remote.UserProfileService
 import nl.gewoonjaap.nfthub.data.remote.dto.UserProfileDataResponse
@@ -19,6 +21,7 @@ class ProfileActivity : AppCompatActivity() {
 
     private var address: String = ""
     private var chain: String = ""
+    private var ProfileImage: ImageView? = null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,8 +31,12 @@ class ProfileActivity : AppCompatActivity() {
         address = intent.getStringExtra(WALLET_ADDRESS).toString()
         chain = intent.getStringExtra(WALLET_CHAIN).toString()
 
-        if(address.isEmpty()) address = "Unknown"
         val addressTextView = findViewById<TextView?>(R.id.WalletAddress)
+        ProfileImage = findViewById(R.id.NFT_Profile_Image)
+
+        if(address.isEmpty()) address = "Unknown"
+
+
             addressTextView.apply {
             text = address
         }
@@ -56,6 +63,10 @@ class ProfileActivity : AppCompatActivity() {
          val userProfile: UserProfileDataResponse? = client.getUserProfile(chain, address)
          if(userProfile != null){
              Toast.makeText(this@ProfileActivity, "Got Data, nfts: ${userProfile.nfts.size}", Toast.LENGTH_LONG).show()
+             if(ProfileImage != null) {
+                 Glide.with(this@ProfileActivity).load(userProfile.nfts.random().metadata.image)
+                     .into(ProfileImage!!)
+             }
          }
          else{
              Toast.makeText(this@ProfileActivity, "Invalid Wallet", Toast.LENGTH_LONG).show()
