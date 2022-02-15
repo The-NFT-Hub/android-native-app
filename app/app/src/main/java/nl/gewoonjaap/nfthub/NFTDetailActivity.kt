717a53c0049c6302a90c73ae28295cecbc03d755
Jaap <33700526+GewoonJaap@users.noreply.github.com>
2022-02-15 13:39:40 +0100

    NFT detail

---
+++ app/app/src/main/java/nl/gewoonjaap/nfthub/NFTDetailActivity.kt
@@ -0,0 +1,11 @@
+package nl.gewoonjaap.nfthub
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NFTDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nftdetail)
+    }
+}
